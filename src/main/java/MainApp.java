2cc7dc79bcc53d28ade221dfb8d5274c3d0a6a3e
Vlad Bychkov <chapaev56@yandex.ru>
2020-04-04 05:13:50 +0400

    Connect to database with configuration from database.properties

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,15 +1,7 @@
-import org.sqlite.JDBC;
-import sql.daoImpl.FactoryDAOImpl;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import sql.ConnectToDB;
 
 public class MainApp {
-    public static void main(String[] args) throws SQLException {
-        System.out.println("hello");
-        DriverManager.registerDriver(new JDBC());
-        System.out.println(FactoryDAOImpl.getAirshipDAO().getAll());
-        FactoryDAOImpl.getAirshipDAO().delete(FactoryDAOImpl.getAirshipDAO().getAll().get(0));
-        System.out.println(FactoryDAOImpl.getAirshipDAO().getAll());
+    public static void main(String[] args) {
+        ConnectToDB.registryDriver();
     }
 }
--- src/main/java/sql/ConnectToDB.java
+++ src/main/java/sql/ConnectToDB.java
@@ -1,22 +1,50 @@
 package sql;
 
+import org.sqlite.JDBC;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectToDB {
 
-    private static final String PATH_TO_DB = "airlines.db";
-
     private static Connection connection;
+    private static String dbType;
+    private static String pathToDb;
+    private static String dbName;
+    private static String dbSchema;
 
     private ConnectToDB() {
+    }
+
+    public static void registryDriver() {
+        try {
+            Properties property = new Properties();
+            property.load(new FileInputStream("database.properties"));
+
+            dbType = property.getProperty("DB_TYPE");
+            pathToDb = property.getProperty("RELATIVE_PATH_TO_DB");
+            dbName = property.getProperty("DB_NAME");
+            dbSchema = property.getProperty("DB_SCHEMA");
+
+            if (dbType.equals("sqlite")) {
+                DriverManager.registerDriver(new JDBC());
+            }
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection("jdbc:sqlite:" + PATH_TO_DB);
+                String path = String.format("jdbc:%s:%s%s", ConnectToDB.dbType, ConnectToDB.pathToDb, ConnectToDB.dbName);
+                connection = DriverManager.getConnection(path);
             }
         } catch (SQLException e) {
             e.printStackTrace();
