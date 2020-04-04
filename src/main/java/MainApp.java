0e1c5fcc9bd50d40e5e36a9f375d2ca1476413ae
Vlad Bychkov <chapaev56@yandex.ru>
2020-04-04 05:52:23 +0400

    Main

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,7 +1,9 @@
 import sql.ConnectToDB;
 
+import java.text.ParseException;
+
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         ConnectToDB.registryDriver();
     }
 }
