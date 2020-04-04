1e69108a9f15b8c797fa3863801a159de6e9293e
Vlad Bychkov <chapaev56@yandex.ru>
2020-04-04 05:07:55 +0400

    Rename parametr of function

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -3,9 +3,9 @@ package dao;
 import java.util.List;
 
 public interface DAO<T> {
-    public abstract void create(T adr);
+    public abstract void create(T obj);
     public abstract T getById(String id);
-    public abstract void update(T adr);
-    public abstract void delete(T adr);
+    public abstract void update(T obj);
+    public abstract void delete(T obj);
     public abstract List<T> getAll();
 }
