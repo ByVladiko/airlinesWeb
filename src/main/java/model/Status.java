77a270b6189ee34f1663d076f49426f9e5e955fb
Vlad Bychkov <chapaev56@yandex.ru>
2020-05-14 05:46:01 +0400

    Modified for database

---
+++ src/main/java/model/Status.java
@@ -0,0 +1,20 @@
+package model;
+
+public enum Status {
+
+    CLOSED, RESERVED, SOLD, FREE;
+
+    public static Status byOrdinal(int ord) {
+        for (Status status : Status.values()) {
+            if (status.ordinal() + 1 == ord) {
+                return status;
+            }
+        }
+        return null;
+    }
+
+    public int getIndex() {
+        return ordinal() + 1;
+    }
+
+}
