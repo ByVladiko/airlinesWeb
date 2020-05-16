6577e161f743fee970f3e8ecca023a028c2a7c94
Vlad Bychkov <chapaev56@yandex.ru>
2020-05-16 07:59:20 +0400

    refactor

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,9 +1,5 @@
-import dao.impl.ClientDAO;
 import dao.impl.TicketDAO;
-import model.Client;
-import model.Ticket;
 import repository.DatabaseConnectivityProvider;
-import service.BusinessLogic;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,13 +7,20 @@ import java.sql.SQLException;
 public class MainApp {
     public static void main(String[] args) {
         DatabaseConnectivityProvider.registryDriver();
+//        try (Connection connection = DatabaseConnectivityProvider.getConnection()) {
+//            BusinessLogic businessLogic = new BusinessLogic();
+//            ClientDAO clientDAO = new ClientDAO();
+//            TicketDAO ticketDAO = new TicketDAO();
+//            Client client = clientDAO.getById(connection, "9ffc9e34-0604-4503-bb29-4a923c030ecf");
+//            Ticket ticket = ticketDAO.getById(connection, "06c96a92-01f6-45b8-a4cc-7d663de30d76");
+//            businessLogic.buyTicketToClient(connection, client, ticket);
+//            System.out.println(clientDAO.getAll(connection).get(0));
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
         try (Connection connection = DatabaseConnectivityProvider.getConnection()) {
-            BusinessLogic businessLogic = new BusinessLogic();
-            ClientDAO clientDAO = new ClientDAO();
             TicketDAO ticketDAO = new TicketDAO();
-            Client client = clientDAO.getById(connection, "9ffc9e34-0604-4503-bb29-4a923c030ecf");
-            Ticket ticket = ticketDAO.getById(connection, "06c96a92-01f6-45b8-a4cc-7d663de30d76");
-            businessLogic.buyTicketToClient(connection, client, ticket);
+            System.out.println(ticketDAO.getAll(connection).get(0).getFlight().getDateOfDeparture().getTime());
         } catch (SQLException e) {
             e.printStackTrace();
         }
