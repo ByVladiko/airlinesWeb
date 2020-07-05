db2808dd7a4d333f8786cd7c52e98513540f0bfa
Vlad Bychkov <chapaev56@yandex.ru>
2020-07-05 04:56:18 +0400

    Edited path to file with properties

---
+++ src/test/java/dao/DatabaseConnect.java
@@ -0,0 +1,57 @@
+package dao;
+
+import org.sqlite.JDBC;
+import org.sqlite.SQLiteConfig;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnect {
+
+    private static String path;
+    private static String dbType;
+    private static String pathToDb;
+    private static String dbName;
+    private static String dbSchema;
+    private static SQLiteConfig sqLiteConfig;
+
+    private static final String PATH_TO_CONNECTION_PROPERTIES = "database.properties";
+
+    private DatabaseConnect() {
+    }
+
+    public static void registryDriver() {
+        try {
+            Properties property = new Properties();
+            property.load(DatabaseConnect.class.getClassLoader().getResource("database.properties").openStream());
+
+            dbType = property.getProperty("DB_TYPE");
+            pathToDb = property.getProperty("RELATIVE_PATH_TO_DB");
+            dbName = property.getProperty("DB_NAME");
+            dbSchema = property.getProperty("DB_SCHEMA");
+
+            sqLiteConfig = new SQLiteConfig();
+            sqLiteConfig.enforceForeignKeys(true);
+
+            if (dbType.equals("sqlite")) {
+                DriverManager.registerDriver(new JDBC());
+                path = String.format("jdbc:%s:%s%s",
+                        DatabaseConnect.dbType,
+                        DatabaseConnect.pathToDb,
+                        DatabaseConnect.dbName);
+            }
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(path, sqLiteConfig.toProperties());
+    }
+
+}
