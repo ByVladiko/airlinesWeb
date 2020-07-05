da9bdfd5807b887c4f6189da524fa71ffb55ff21
Vlad Bychkov <chapaev56@yandex.ru>
2020-07-05 18:56:11 +0400

    MainClass

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -7,20 +7,9 @@ import java.sql.SQLException;
 public class MainApp {
     public static void main(String[] args) {
         DatabaseConnectivityProvider.registryDriver();
-//        try (Connection connection = DatabaseConnectivityProvider.getConnection()) {
-//            BusinessLogic businessLogic = new BusinessLogic();
-//            ClientDAO clientDAO = new ClientDAO();
-//            TicketDAO ticketDAO = new TicketDAO();
-//            Client client = clientDAO.getById(connection, "9ffc9e34-0604-4503-bb29-4a923c030ecf");
-//            Ticket ticket = ticketDAO.getById(connection, "06c96a92-01f6-45b8-a4cc-7d663de30d76");
-//            businessLogic.buyTicketToClient(connection, client, ticket);
-//            System.out.println(clientDAO.getAll(connection).get(0));
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
         try (Connection connection = DatabaseConnectivityProvider.getConnection()) {
             TicketDAO ticketDAO = new TicketDAO();
-            System.out.println(ticketDAO.getAll(connection).get(0).getFlight().getDateOfDeparture().getTime());
+            System.out.println(ticketDAO.getAll(connection).get(0).getFlight().getDateOfDeparture());
         } catch (SQLException e) {
             e.printStackTrace();
         }
