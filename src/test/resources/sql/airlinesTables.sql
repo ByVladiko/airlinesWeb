dcd3b197080d6057e870a1ab4852542acacd6719
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-02 03:01:39 +0400

    removed useless .sql scripts

--- src/test/resources/sql/airlinesTables.sql
+++ src/test/resources/sql/airlinesTables.sql
@@ -90,4 +90,15 @@ CREATE TABLE ticket (
                                                  ON UPDATE CASCADE
 );
 
+INSERT INTO category (id, name) VALUES (1, 'ECONOMY');
+INSERT INTO category (id, name) VALUES (2, 'PREMIUM');
+INSERT INTO category (id, name) VALUES (3, 'BUSINESS');
+
+-- Table: status
+
+INSERT INTO status (id_status, name) VALUES (1, 'CLOSED');
+INSERT INTO status (id_status, name) VALUES (2, 'RESERVED');
+INSERT INTO status (id_status, name) VALUES (3, 'SOLD');
+INSERT INTO status (id_status, name) VALUES (4, 'FREE');
+
 PRAGMA foreign_keys = on;
