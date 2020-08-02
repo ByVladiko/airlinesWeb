740d849258cf77e1f55b2969d5b5bbf1446cab28
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-02 16:58:23 +0400

    class impl CrudRepository

---
+++ src/main/java/repository/AirshipRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Airship;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AirshipRepository extends CrudRepository<Airship, Integer> {
+
+}
