dfc374a36fb2304b896b9d798e1ad23734c8da02
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-19 02:55:14 +0400

    update

--- src/main/java/com/airlines/repository/AirshipRepository.java
+++ src/main/java/com/airlines/repository/AirshipRepository.java
@@ -3,13 +3,18 @@ package com.airlines.repository;
 import com.airlines.model.Airship;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@RestController
 public interface AirshipRepository extends CrudRepository<Airship, UUID> {
 
     @Override
-    Optional<Airship> findById(UUID uuid);
+    @GetMapping(path = "airships/findById")
+    Optional<Airship> findById(@RequestParam UUID id);
 }
