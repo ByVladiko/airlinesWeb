3026b66540e43d1ed20e7de6e74b26fd2bc96a64
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-23 15:21:32 +0400

    custom detail service

---
+++ src/main/java/com/airlines/service/CustomUserDetailService.java
@@ -0,0 +1,42 @@
+package com.airlines.service;
+
+import com.airlines.model.user.Role;
+import com.airlines.model.user.User;
+import com.airlines.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        org.springframework.security.core.userdetails.User.
+                UserBuilder builder;
+        if (user != null) {
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
+            builder.roles(Arrays.toString(Role.values()));
+        } else {
+            throw new UsernameNotFoundException("User not found.");
+        }
+
+        return builder.build();
+    }
+}
