f5cf776c1b6a338fa3188c92b78a43949dc2548c
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-23 15:23:06 +0400

    user authentication exceptions

---
+++ src/main/java/com/airlines/service/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.airlines.service.exception;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(final String msg) {
+        super(msg);
+    }
+
+}
