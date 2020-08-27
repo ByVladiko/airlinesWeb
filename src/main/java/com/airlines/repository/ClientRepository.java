86ce36784c21e911af79fa4119139c1d9d00a671
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-27 05:16:30 +0400

    added List<Ticket> findAllById(UUID id)

--- src/main/java/com/airlines/repository/ClientRepository.java
+++ src/main/java/com/airlines/repository/ClientRepository.java
@@ -1,15 +1,20 @@
 package com.airlines.repository;
 
+import com.airlines.exception.UserNotFoundException;
 import com.airlines.model.airship.Client;
+import com.airlines.model.airship.Ticket;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ClientRepository extends CrudRepository<Client, UUID> {
+public interface ClientRepository extends CrudRepository<Client, UUID>, CustomClientRepository {
 
     @Override
     Optional<Client> findById(UUID uuid);
+
+    List<Ticket> findAllById(UUID id) throws UserNotFoundException;
 }
