f355e0f3eefd66611a2b5a34a52f8e7b361b3d28
Vlad Bychkov <chapaev56@yandex.ru>
2020-08-28 06:01:30 +0400

    controller for purchase ticket to client

---
+++ src/main/java/com/airlines/controller/BuyingTicket.java
@@ -0,0 +1,26 @@
+package com.airlines.controller;
+
+import com.airlines.model.airship.Ticket;
+import com.airlines.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+@Controller
+public class BuyingTicket {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @GetMapping(path = "/purchase/client{id}")
+    public String greeting(@PathVariable("id") String id, Map<String, Object> params) {
+        Iterable<Ticket> tickets = ticketRepository.findAll();
+        params.put("tickets", tickets);
+        fillFields(params);
+        return "tickets";
+    }
+
+}
