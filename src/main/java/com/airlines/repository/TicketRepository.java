35a3b938b4e7275e72329c06b9adcbc5ec832853
Vlad Bychkov <chapaev56@yandex.ru>
2020-09-01 05:57:27 +0400

    ticket repository

--- src/main/java/com/airlines/repository/TicketRepository.java
+++ src/main/java/com/airlines/repository/TicketRepository.java
@@ -1,9 +1,14 @@
 package com.airlines.repository;
 
+import com.airlines.model.airship.Category;
+import com.airlines.model.airship.Flight;
+import com.airlines.model.airship.Status;
 import com.airlines.model.airship.Ticket;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +17,8 @@ public interface TicketRepository extends CrudRepository<Ticket, UUID> {
 
     @Override
     Optional<Ticket> findById(UUID uuid);
+
+    List<Ticket> findTicketsByStatus(Status status);
+
+    List<Ticket> findTicketsByStatusAndFlightAndCategory(Status status, @Nullable Flight flight, @Nullable Category category);
 }
